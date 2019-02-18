6282e4d56e733a23ebb6b35da67e8c38190c604f
SpartHunter <peckogloire@gmail.com>
2019-02-19 00:13:34 +0100

    Add cors to code 3

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ const server = restify.createServer();
 //Middleware
 
 server.use(restify.plugins.bodyParser());
-server.use(
-	function crossOrigin(req, res, next) {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "X-Requested-With");
-		return next();
-	}
-);
+
+server.pre((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', req.header('origin'));
+	res.header('Access-Control-Allow-Headers', req.header('Access-Control-Request-Headers'));
+	res.header('Access-Control-Allow-Credentials', 'true');
+	if (req.method === 'OPTIONS')
+		return res.send(204);
+	next();
+});
 server.listen(config.PORT, () => {
 	mongoose.set('useFindAndModify', false);
 	mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true})
